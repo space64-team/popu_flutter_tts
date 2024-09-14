39a1e7c495f694705d06781a848941fcc11dd27e
duc0 <duc@space64.xyz>
2024-09-14 09:14:39 +0700

    Create popu_lib_tts.dart

---
+++ lib/popu_lib_tts.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+import 'flutter_tts.dart';
+
+class PopuTtsService {
+  static PopuTtsService instance = PopuTtsService();
+
+  FlutterTts tts = FlutterTts();
+  List<dynamic> languages = [];
+
+  Future<void> init(String language) async {
+    await tts.setLanguage(language);
+    languages = await tts.getLanguages as List<String>;
+  }
+}
+
+class PopuTtsTroubleshootScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('Tts')),
+        body: SizedBox(
+            height: 500,
+            child: GridView.count(
+              crossAxisCount: 4,
+              childAspectRatio: 1,
+              // Number of columns in the grid
+              padding: const EdgeInsets.all(16.0),
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              children: PopuTtsService.instance.languages.map((x) =>
+                  _buildGridItem(context, x.toString())
+              ).toList(),
+            )));
+  }
+
+  Widget _buildGridItem(
+      BuildContext context, String language) {
+    return Container(
+        decoration: BoxDecoration(
+          color: Colors.blueAccent,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Center(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Text(
+              language,
+              style: const TextStyle(fontSize: 18, color: Colors.white),
+              textAlign: TextAlign.center,
+            )
+          ]),
+        )
+    );
+  }
+}
