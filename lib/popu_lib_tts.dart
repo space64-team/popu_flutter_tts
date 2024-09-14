8a2d1463a0d023c4c9f18ff8b9948d06a1d6a0f4
duc0 <duc@space64.xyz>
2024-09-14 11:18:18 +0700

    LLgs

--- lib/popu_lib_tts.dart
+++ lib/popu_lib_tts.dart
@@ -1,17 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:popu_flutter_tts/flutter_tts.dart';
 import 'package:popu_lib_core/popu_lib_core.dart';
-
-import 'flutter_tts.dart';
 
 class PopuTtsService {
   static PopuTtsService instance = PopuTtsService();
 
+  String language = "";
+  FlutterTts? tts;
+
   Future<void> init(String language) async {
     tts = FlutterTts();
+    this.language = language;
     await tts?.setLanguage(language);
   }
 
-  FlutterTts? tts;
+  Future<List<String>> getVoices() async {
+    var vRaw = await tts?.getVoices;
+    var voices = vRaw as List<dynamic>;
+    List<String> ret = [];
+    for (var v in voices) {
+      if (v['locale'] == language) {
+        ret.add(v['name'].toString());
+      }
+    }
+    ret.sort((a, b) => a.compareTo(b));
+    PopuLogging.logger.w('Voices ${ret.toString()}');
+    return ret;
+  }
 
   Future<List<PopuTtsSupportedLanguage>> getTroubleshootData() async {
     List<PopuTtsSupportedLanguage> ret = [];
@@ -20,17 +35,15 @@ class PopuTtsService {
     PopuLogging.logger.w('Languages $lRaw');
     PopuLogging.logger.w('Voices $vRaw');
     var languages = lRaw as List<dynamic>;
-    
+
     for (var l in languages) {
       ret.add(PopuTtsSupportedLanguage(l.toString()));
     }
-    var voices = vRaw as List<Map<dynamic, dynamic>>;
+    var voices = vRaw as List<dynamic>;
     for (var v in voices) {
-      for (var entry in v.entries) {
-        for (var l in ret) {
-          if (l.langCode == entry.value.toString()) {
-            l.voices.add(entry.key.toString());
-          }
+      for (var l in ret) {
+        if (l.langCode == v['locale']) {
+          l.voices.add(v['name'].toString());
         }
       }
     }
@@ -40,6 +53,10 @@ class PopuTtsService {
 
   Future<void> speak(String text) async {
     await tts?.speak(text);
+  }
+
+  void changeVoice(String voice) async {
+    await tts?.setVoice({"name": voice, "locale": language});
   }
 }
 
@@ -56,6 +73,8 @@ class PopuTtsSupportedLanguage {
 }
 
 class PopuTtsTroubleshootScreen extends StatefulWidget {
+  const PopuTtsTroubleshootScreen({super.key});
+
   @override
   State<PopuTtsTroubleshootScreen> createState() =>
       _PopuTtsTroubleshootScreenState();
@@ -68,24 +87,22 @@ class _PopuTtsTroubleshootScreenState extends State<PopuTtsTroubleshootScreen> {
   void initState() {
     super.initState();
     PopuTtsService.instance.getTroubleshootData().then((v) => {
-          setState(() {
-            _data = v;
-          })
-        });
+      setState(() {
+        _data = v;
+      })
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: const Text('Tts')),
-        body: SizedBox(
-            height: 500,
-            child: ListView(
-              padding: const EdgeInsets.all(8),
-              children: _data
-                  .map((x) => _buildGridItem(context, x.toString()))
-                  .toList(),
-            )));
+        body: ListView(
+          padding: const EdgeInsets.all(8),
+          children: _data
+              .map((x) => _buildGridItem(context, x.toString()))
+              .toList(),
+        ));
   }
 
   Widget _buildGridItem(BuildContext context, String language) {
@@ -103,5 +120,53 @@ class _PopuTtsTroubleshootScreenState extends State<PopuTtsTroubleshootScreen> {
             )
           ]),
         ));
+  }
+}
+
+class PopuVoiceChangeDialog extends StatefulWidget {
+  const PopuVoiceChangeDialog({super.key});
+
+  @override
+  State<PopuVoiceChangeDialog> createState() => _PopuVoiceChangeDialogState();
+}
+
+class _PopuVoiceChangeDialogState extends State<PopuVoiceChangeDialog> {
+  List<String> _voices = [];
+
+  @override
+  void initState() {
+    super.initState();
+    PopuTtsService.instance.getVoices().then((x) => {
+      setState(() {
+        _voices = x;
+      })
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Change Speech Voice'),
+      content: SizedBox(
+        width: double.maxFinite,
+        child: ListView.builder(
+          shrinkWrap: true,
+          itemCount: _voices.length,
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              title: Text(_voices[index]),
+              onTap: () {
+                _changeVoice(_voices[index]);
+                Navigator.of(context).pop();
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  void _changeVoice(String voice) {
+    PopuTtsService.instance.changeVoice(voice);
   }
 }
